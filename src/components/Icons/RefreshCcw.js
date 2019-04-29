47e31b490d87c893f87936cf4f7d462c4bcc8151
Dimitris <dimitrisraptis96@gmail.com>
2019-04-29 16:37:07 +0300

    add some icons

---
+++ src/components/Icons/RefreshCcw.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SvgRefreshCcw = props => (
+  <svg
+    width={24}
+    height={24}
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="refresh-ccw_svg__feather refresh-ccw_svg__feather-refresh-ccw"
+    {...props}
+  >
+    <path d="M1 4v6h6M23 20v-6h-6" />
+    <path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15" />
+  </svg>
+);
+
+export default SvgRefreshCcw;
