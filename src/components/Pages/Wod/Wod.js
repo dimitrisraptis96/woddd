1f4436ffb005f29ab6c7f686a29af2c1dade2532
Dimitris <dimitrisraptis96@gmail.com>
2019-05-10 22:23:21 +0300

    fix layout broken scroll style

--- src/components/Pages/Wod/Wod.js
+++ src/components/Pages/Wod/Wod.js
@@ -3,10 +3,26 @@ import React from "react";
 import Button from "../../UI/Button";
 import Card from "../../UI/Card";
 import RefreshIcon from "../../UI/Icons/RefreshOutline";
+import { Header1, Body } from "../../UI/Typography";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  box-sizing: border-box;
+
+  h1 {
+    margin-bottom: 1rem;
+  }
+`;
 
 const Wod = ({ wod, saveWod, removeWod, isLiked, generate }) => {
   return (
-    <React.Fragment>
+    <Container>
+      <Header1 weight="bold">Random WOD</Header1>
       <Card
         title={wod.title}
         type={wod.type}
@@ -20,7 +36,7 @@ const Wod = ({ wod, saveWod, removeWod, isLiked, generate }) => {
         <RefreshIcon />
         Generate Again
       </Button>
-    </React.Fragment>
+    </Container>
   );
 };
 
--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Body = styled.div`
-  width: 100vw;
+  /* width: 100vw; */
   box-sizing: border-box;
 
   padding: 2rem 4rem;
@@ -10,6 +10,8 @@ const Body = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  padding-bottom: 8rem;
 `;
 
 export default Body;
