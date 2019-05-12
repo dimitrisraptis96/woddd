e79960ae6090ff1b2eee6120e37ceab0f81f7db7
Dimitris <dimitrisraptis96@gmail.com>
2019-05-12 20:42:27 +0300

    add card layout and style cards

--- src/components/Pages/Favorites/Favorites.js
+++ src/components/Pages/Favorites/Favorites.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { rgba } from "polished";
 
 import { Header1, Body } from "../../UI/Typography";
+import Card from "../../UI/Card";
+
+import theme from "../../../utils/theme";
+const { secondary } = theme.colors;
 
 const Container = styled.div`
   display: flex;
@@ -18,30 +23,62 @@ const Container = styled.div`
 `;
 
 const WodsList = styled.div`
+  --num-of-cards: 0;
+
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  width: 100%;
+  flex-wrap: wrap;
+  list-style: none;
+
+  @media (max-width: 28rem) {
+    flex: 0 1 100%;
+    min-width: 100%;
+  }
+  @media (min-width: 60rem) {
+    width: ${5 * 16 + 6 * 150}px;
+    min-width: ${5 * 16 + 6 * 150}px;
+    max-width: ${5 * 16 + 6 * 150}px;
+  }
 
   color: ${props => props.theme.colors.tertiary};
   font-weight: bold;
 
-  > * {
-    margin-top: 1rem;
-  }
-  > *:first-child {
-    margin-top: 0;
-  }
+  margin: 0;
+  padding: 0;
 `;
-const Favorites = ({ wods }) => {
+
+const WodCard = styled.div`
+  box-sizing: border-box;
+  background-color: ${secondary};
+  border-radius: 1rem;
+  box-shadow: 0px 8px ${rgba(secondary, 0.5)}, 0px 16px ${rgba(secondary, 0.4)},
+    0px 24px ${rgba(secondary, 0.3)};
+
+  text-align: center;
+
+  padding: 1rem;
+  margin: 0.5rem;
+  margin-bottom: ${25 + 16}px;
+
+  width: 150px;
+`;
+
+const Favorites = ({ wods, handleWod }) => {
   return (
     <Container>
-      <Header1 weight="bold">My Favorites Wods</Header1>
-      <Body>Here you can manage your saved WODs.</Body>
+      <Header1 weight="bold" color="tertiary">
+        Favorites
+      </Header1>
+      <Body>Here you can manage your favorite WODs.</Body>
       <WodsList>
         {wods.map(wod => (
-          <div key={wod}>{wod.split("-")[0]}</div>
+          <WodCard
+            key={wod}
+            onClick={() => handleWod(wod)}
+            style={{ cursor: "pointer" }}
+          >
+            {wod.split("-")[0]}
+          </WodCard>
         ))}
       </WodsList>
     </Container>
