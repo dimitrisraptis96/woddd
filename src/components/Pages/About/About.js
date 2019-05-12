9473cab4d4e992f28d29e003e61897e02e327ffb
Dimitris <dimitrisraptis96@gmail.com>
2019-05-12 20:43:58 +0300

    add layout and card styling

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { rgba } from "polished";
 
-import { Header1, Body } from "../../UI/Typography";
+import { Header1, Body, Small } from "../../UI/Typography";
+
+import theme from "../../../utils/theme";
+
+const { secondary } = theme.colors;
 
 const Container = styled.div`
   display: flex;
@@ -11,39 +16,73 @@ const Container = styled.div`
   width: 100%;
   box-sizing: border-box;
 
-  h1 {
+  > h1 {
     margin-bottom: 1rem;
   }
-  p {
+  > p {
     margin-bottom: 2rem;
   }
 `;
 
 const WodsList = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  width: 100%;
+  flex-wrap: wrap;
+  list-style: none;
+
+  @media (max-width: 28rem) {
+    flex: 0 1 100%;
+    min-width: 100%;
+  }
+  @media (min-width: 60rem) {
+    width: ${5 * 16 + 6 * 150}px;
+    min-width: ${5 * 16 + 6 * 150}px;
+    max-width: ${5 * 16 + 6 * 150}px;
+  }
 
   color: ${props => props.theme.colors.tertiary};
   font-weight: bold;
-  > * {
-    margin-top: 1rem;
-  }
-  > *:first-child {
-    margin-top: 0;
+
+  margin: 0;
+  padding: 0;
+`;
+
+const WodCard = styled.div`
+  box-sizing: border-box;
+  background-color: ${secondary};
+  border-radius: 1rem;
+  box-shadow: 0px 8px ${rgba(secondary, 0.5)}, 0px 16px ${rgba(secondary, 0.4)},
+    0px 24px ${rgba(secondary, 0.3)};
+
+  text-align: center;
+
+  padding: 1rem;
+  margin: 0.5rem;
+  margin-bottom: ${25 + 16}px;
+
+  width: 150px;
+  cursor: pointer;
+
+  > p:first-child {
+    margin-bottom: 0.5rem;
   }
 `;
 
-const About = ({ wods }) => {
+const About = ({ wods, handleWod }) => {
   return (
     <Container>
-      <Header1 weight="bold">About</Header1>
+      <Header1 weight="bold" color="tertiary">
+        About
+      </Header1>
       <Body>Display some info about the back story of the app and me.</Body>
       <WodsList>
         {wods.map(wod => (
-          <div key={wod.id}>{wod.title}</div>
+          <WodCard key={wod.id} onClick={() => handleWod(wod.id)}>
+            <Body weight="bold" color="tertiary">
+              {wod.title}
+            </Body>
+            <Small color="white">{wod.type}</Small>
+          </WodCard>
         ))}
       </WodsList>
     </Container>
