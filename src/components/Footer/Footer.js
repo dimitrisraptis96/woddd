d4073547ac70eef7519524365c58871d56e8cc62
Dimitris <dimitrisraptis96@gmail.com>
2019-05-18 15:22:02 +0300

    use Link component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Small } from "../../components/UI/Typography";
-import theme from "../../utils/theme";
+import Link from "../UI/Link";
+import { Small } from "../UI/Typography";
 
 const Container = styled.div`
   position: fixed;
@@ -19,56 +19,12 @@ const Container = styled.div`
   background-color: ${props => props.theme.colors.primary};
 `;
 
-const A = styled.a`
-  color: ${props => props.theme.colors.tertiary};
-  font-weight: bold;
-  font-size: 0.75rem;
-  text-decoration: none;
-
-  &:focus {
-    outline-color: ${props => props.theme.colors.tertiary};
-    padding: 0.25rem;
-  }
-`;
-
-const Highlight = styled.span`
-  position: relative;
-  z-index: 1;
-
-  &:before {
-    content: "";
-    position: absolute;
-    z-index: -1;
-    top: 0;
-    bottom: 0;
-    left: -0.25em;
-    right: -0.25em;
-    background-color: ${props => props.theme.colors.tertiary};
-    transform-origin: center right;
-    transform: scaleX(0);
-    transition: transform 0.2s ease-in-out;
-  }
-
-  &:hover::before {
-    transform: scaleX(1);
-    transform-origin: center left;
-  }
-
-  &:hover {
-    color: ${props => props.theme.colors.primary};
-  }
-`;
-
-const Footer = ({ baseColor }) => {
+const Footer = ({}) => {
   return (
     <Container>
       <Small>
         Designed and Built by{" "}
-        <A href="https://twitter.com/d__raptis">
-          <Highlight>
-            <b>@draptis</b>
-          </Highlight>
-        </A>
+        <Link href="https://twitter.com/d__raptis">@draptis</Link>
       </Small>
     </Container>
   );
