337950e95dba061bfdf22ab3f976577e4792fcbc
Dimitris <dimitrisraptis96@gmail.com>
2019-05-18 15:22:29 +0300

    update about page text

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -2,11 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import { rgba } from "polished";
 
-import { Header1, Body, Small } from "../../UI/Typography";
+import Link from "../../UI/Link";
+import { Header1, Header2, Body, Small } from "../../UI/Typography";
 
 import theme from "../../../utils/theme";
-
-const { secondary } = theme.colors;
 
 const Container = styled.div`
   display: flex;
@@ -17,74 +16,58 @@ const Container = styled.div`
   box-sizing: border-box;
 
   > h1 {
-    margin-bottom: 1rem;
+    margin-bottom: 3rem;
+  }
+  > h2 {
+    margin-bottom: 0.5rem;
+    align-self: flex-start;
   }
   > p {
-    margin-bottom: 2rem;
+    line-height: 1.7;
+    margin-bottom: 3rem;
   }
 `;
 
-const WodsList = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  list-style: none;
-
-  @media (max-width: 28rem) {
-    flex: 0 1 100%;
-    min-width: 100%;
-  }
-  @media (min-width: 60rem) {
-    width: ${5 * 16 + 6 * 150}px;
-    min-width: ${5 * 16 + 6 * 150}px;
-    max-width: ${5 * 16 + 6 * 150}px;
-  }
-
-  color: ${props => props.theme.colors.tertiary};
-  font-weight: bold;
-
-  margin: 0;
-  padding: 0;
-`;
-
-const WodCard = styled.div`
-  box-sizing: border-box;
-  background-color: ${secondary};
-  border-radius: 1rem;
-  box-shadow: 0px 8px ${rgba(secondary, 0.5)}, 0px 16px ${rgba(secondary, 0.4)},
-    0px 24px ${rgba(secondary, 0.3)};
-
-  text-align: center;
-
-  padding: 1rem;
-  margin: 0.5rem;
-  margin-bottom: ${25 + 16}px;
-
-  width: 150px;
-  cursor: pointer;
-
-  > p:first-child {
-    margin-bottom: 0.5rem;
-  }
-`;
-
-const About = ({ wods, handleWod }) => {
+const About = props => {
   return (
     <Container>
       <Header1 weight="bold" color="tertiary">
         About
       </Header1>
-      <Body>Display some info about the back story of the app and me.</Body>
-      <WodsList>
-        {wods.map(wod => (
-          <WodCard key={wod.id} onClick={() => handleWod(wod.id)}>
-            <Body weight="bold" color="tertiary">
-              {wod.title}
-            </Body>
-            <Small color="white">{wod.type}</Small>
-          </WodCard>
-        ))}
-      </WodsList>
+
+      <Header2 color="tertiary" align="left" weight="bold">
+        What is Woddd?
+      </Header2>
+      <Body align="left">
+        Woddd generator was created for Crossfit enthusiasts or guys hungry of a
+        crazy 15-minute routine. By generating a random wod, the struggle of
+        searching hours for a matching workout is not a problem anymore. The
+        goal of Woddd generator is to help fitness fans break out of the habit
+        of using the same routine and beat workout boredom.
+      </Body>
+
+      <Header2 color="tertiary" align="left" weight="bold">
+        How does Woddd work?
+      </Header2>
+      <Body align="left">
+        Woddd is not rocket science! It works as simple as that. Each time the
+        "Generate" button is pressed, a random wod is picked from our database.
+        If you like a suggested wod you can easily save it to your favorites and
+        access it again anytime in the future. For those more interested, feel
+        free to take a look at the project on{" "}
+        <Link href="https://github.com/dimitrisraptis96/woddd">GitHub</Link>.
+      </Body>
+
+      <Header2 color="tertiary" align="left" weight="bold">
+        Who created Woddd?
+      </Header2>
+      <Body align="left">
+        Woddd was created as a fun side project by{" "}
+        <Link href="https://www.raptis.wtf">Dimitris Raptis</Link> who used to
+        search for hours and kill wods every Sunday. Dimitris is the co-founder
+        of Loceye and a fitness freak. User Interface design and Front-end
+        development are his geek interests.
+      </Body>
     </Container>
   );
 };
