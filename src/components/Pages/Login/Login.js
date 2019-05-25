40cef816e5ecf61c950592d51553822dc1e65904
Dimitris <dimitrisraptis96@gmail.com>
2019-05-25 13:26:25 +0300

    add guest option

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import Button from "../../UI/Button";
+import Column from "../../UI/Column";
 import Logo from "../../UI/Logo";
 import { Body } from "../../UI/Typography";
 import FacebookOutlineIcon from "../../UI/Icons/FacebookOutline";
+
+const StyledLink = styled(Link)`
+  color: white;
+
+  &:hover {
+    color: red;
+  }
+`;
 
 const Login = ({ signin }) => (
   <React.Fragment>
@@ -11,10 +22,14 @@ const Login = ({ signin }) => (
     <Body style={{ marginBottom: "2rem" }}>
       Generate a random Wod and kill it!
     </Body>
-    <Button onClick={signin}>
-      <FacebookOutlineIcon />
-      Sign in with Facebook
-    </Button>
+    <Column>
+      <Button onClick={signin}>
+        <FacebookOutlineIcon />
+        Sign in with Facebook
+      </Button>
+      <Body color="secondary">or</Body>
+      <StyledLink to="/guest">Continue as Guest</StyledLink>
+    </Column>
   </React.Fragment>
 );
 
